97764132030c6172d201a7b07325698a9e8a338e
Dildora Muslimova <faithpacifica@yahoo.com>
2026-01-28 08:17:06 +0900

    feat:add routes , create Navigation and style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom"; // BrowserRouter bu degani routing uchun kerak boladi, ya'ni sahifalar orasida yurish uchun ,generic router leverages URL and keep track of the history of where the user is navigating trhough
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
